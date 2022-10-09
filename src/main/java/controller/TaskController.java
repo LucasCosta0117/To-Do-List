cec2408163f83e99436ed456fc5f44e5efee8ce8
Lucas Costa <lucas.costa0117@gmail.com>
2022-10-08 23:41:27 -0300

    Add new class TaskCrontroller on controller package

---
+++ src/main/java/controller/TaskController.java
@@ -0,0 +1,25 @@
+package controller;
+
+import model.Task;
+import java.util.List;
+
+public class TaskController {
+
+    public void save(Task task){
+        //Método para inserir no banco de dados (INSERT)
+    }
+
+    public void update(Task task){
+        //Método para atualizar o banco de dados (UPDATE)
+    }
+
+    public void removeById(int taskId){
+        //Método para deletar no banco de dados(DELETE)
+    }
+
+    public List<Task> getAll(int idProject){
+        //Método para retornar todas as tarefas do banco de dados (SELECT)
+        return null;
+    }
+
+}
