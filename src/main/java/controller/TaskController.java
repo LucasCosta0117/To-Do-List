403cac33b82ba47ed0f6c55df3f985ad8ef7228f
Lucas Costa <lucas.costa0117@gmail.com>
2022-10-11 20:48:12 -0300

    Add a new method removeById on class TaskController

--- src/main/java/controller/TaskController.java
+++ src/main/java/controller/TaskController.java
@@ -1,6 +1,11 @@
 package controller;
 
 import model.Task;
+import util.ConnectionFactory;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.List;
 
 public class TaskController {
@@ -13,8 +18,22 @@ public class TaskController {
         //Método para atualizar o banco de dados (UPDATE)
     }
 
-    public void removeById(int taskId){
+    public void removeById(int taskId) throws SQLException {
         //Método para deletar no banco de dados(DELETE)
+        String sql = "DELETE FROM task WHERE id = ?";
+        Connection conn = null;
+        PreparedStatement statement = null;
+
+        try {
+            conn = ConnectionFactory.getConnection();
+            statement = conn.prepareStatement(sql);
+            statement.setInt(1, taskId);
+            statement.execute();
+        } catch ( SQLException exception ) {
+            throw new SQLException("Erro ao deletar a tarefa", exception);
+        } finally {
+            ConnectionFactory.closeConnection(conn);
+        }
     }
 
     public List<Task> getAll(int idProject){
