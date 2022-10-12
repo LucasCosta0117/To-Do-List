a8c06019ceaabe31430a73c47b5f70938faefce7
Lucas Costa <lucas.costa0117@gmail.com>
2022-10-11 21:34:49 -0300

    Add new method save() on class TaskController

--- src/main/java/controller/TaskController.java
+++ src/main/java/controller/TaskController.java
@@ -4,14 +4,44 @@ import model.Task;
 import util.ConnectionFactory;
 
 import java.sql.Connection;
+import java.sql.Date;
 import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.List;
 
 public class TaskController {
 
-    public void save(Task task){
-        //Método para inserir no banco de dados (INSERT)
+    public void save(Task task) throws SQLException {
+        //Método para inserir registros no banco de dados (INSERT)
+        String sql = "INSERT INTO task " +
+                "(idProject," +
+                "name," +
+                "description," +
+                "notes," +
+                "isCompleted," +
+                "deadline," +
+                "createdAt," +
+                "updatedAt) VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
+        Connection conn = null;
+        PreparedStatement statement = null;
+
+        try {
+            conn = ConnectionFactory.getConnection();
+            statement = conn.prepareStatement(sql);
+            statement.setInt(1, task.getIdProject());
+            statement.setString(2, task.getName());
+            statement.setString(3, task.getDescription());
+            statement.setString(4, task.getNotes());
+            statement.setBoolean(5, task.isCompleted());
+            statement.setDate(6, new Date(task.getDeadLine().getTime()));
+            statement.setDate(7, new Date(task.getCreatedAt().getTime()));
+            statement.setDate(8, new Date(task.getUpdatedAt().getTime()));
+            statement.execute();
+        } catch (SQLException exception) {
+            throw new SQLException("Erro ao inserir uma nova tarefa", exception);
+        } finally {
+            ConnectionFactory.closeConnection(conn);
+        }
     }
 
     public void update(Task task){
