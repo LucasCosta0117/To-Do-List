840b607695c940569be4c3e764496daf6098ac8d
Lucas Costa <lucas.costa0117@gmail.com>
2022-10-11 23:31:34 -0300

    Add method closeConnection() with new resultSet param

--- src/main/java/util/ConnectionFactory.java
+++ src/main/java/util/ConnectionFactory.java
@@ -2,6 +2,7 @@ package util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.Statement;
 
 public class ConnectionFactory {
@@ -36,6 +37,22 @@ public class ConnectionFactory {
             }
             if (statement != null){
                 statement.close();
+            }
+        } catch (Exception exception) {
+            throw new RuntimeException ("Erro ao tentar fechar a conexão com o Banco de Dados", exception);
+        }
+    }
+
+    public static void closeConnection(Connection connection, Statement statement, ResultSet resultSet){
+        try{
+            if (connection != null){
+                connection.close();
+            }
+            if (statement != null){
+                statement.close();
+            }
+            if (resultSet != null){
+                resultSet.close();
             }
         } catch (Exception exception) {
             throw new RuntimeException ("Erro ao tentar fechar a conexão com o Banco de Dados", exception);
