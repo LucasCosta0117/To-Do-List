3b62db45cb6d3637e4334072410f21f89de61d42
Lucas Costa <lucas.costa0117@gmail.com>
2022-10-15 17:18:51 -0300

    Add a Hands-on test for Controllers

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,13 +1,19 @@
 package main;
 
 import controller.ProjectController;
+import controller.TaskController;
 import model.Project;
+import model.Task;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        //Hands-on test
+    public static void main(String[] args) throws ParseException {
+        //Hands-on test Project
+
         Project project = new Project();
         ProjectController projectController = new ProjectController();
 
@@ -32,5 +38,43 @@ public class Main {
         // 4º test for a delete method
         project.setId(1); //Look at ID from MySQL after save method
         projectController.removeById(project.getId());
+
+
+        // Hands-on test Task
+        Task task = new Task();
+        TaskController taskController = new TaskController();
+
+        // 1º test for save method;
+        String data = "01/01/2222";
+        SimpleDateFormat f = new SimpleDateFormat("dd/MM/yyyy");
+        Date data2 = f.parse(data);
+
+        task.setIdProject(2); //Look at ID from MySQL after save method project
+        task.setName("Teste 2");
+        task.setDescription("Descrição teste 2");
+        task.setNotes("Fazer fazer");
+        task.setCompleted(true);
+        task.setDeadLine(data2);
+
+        taskController.save(task);
+
+
+        // 2º test for update method;
+        task.setIdProject(2); //Look at ID from MySQL after save method project
+        task.setName("Tarefa teste Update");
+        task.setDescription("Descrição teste Uptade");
+        task.setNotes("Fazer tal atividade");
+        task.setCompleted(false);
+        task.setDeadLine(data2);
+        task.setId(1);
+
+        taskController.update(task);
+
+        // 3º test for gellAll method;
+        List<Task> taskList = taskController.getAll(2);
+        System.out.println("Tasks: " + taskList);
+
+        // 4º test for delete method
+        taskController.removeById(1); //Look at ID from MySQL after save method project
     }
 }
