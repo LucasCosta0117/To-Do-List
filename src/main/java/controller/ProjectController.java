5a253cf3796af53844bf9cd4bfb240570c70f4f8
Lucas Costa <lucas.costa0117@gmail.com>
2022-10-14 23:39:08 -0300

    Add new method save on class ProjectController

--- src/main/java/controller/ProjectController.java
+++ src/main/java/controller/ProjectController.java
@@ -1,27 +1,52 @@
 package controller;
 
-import model.Task;
+import model.Project;
+import util.ConnectionFactory;
 
+import java.sql.*;
 import java.util.List;
 
 public class ProjectController {
 
-    public void save (Task task) {
+    public void save (Project project) {
         //Método para inserir registros no banco de dados (INSERT)
+        String sql = "INSERT INTO project " +
+                "(id," +
+                "name," +
+                "description, " +
+                "createdAt," +
+                "updatedAt ) " +
+                "VALUES (?, ?, ?, ?, ?);";
+        Connection conn = null;
+        PreparedStatement statement = null;
 
+        try {
+            conn = ConnectionFactory.getConnection();
+            statement = conn.prepareStatement(sql);
+            statement.setInt(1, project.getId());
+            statement.setString(2, project.getName());
+            statement.setString(3, project.getDescription());
+            statement.setDate(4, new Date(project.getCreatedAt().getTime()));
+            statement.setDate(5, new Date(project.getUpdatedAt().getTime()));
+            statement.execute();
+        } catch (Exception exception){
+            throw new RuntimeException ("Erro ao tentar inserir novo projeto" ,exception);
+        } finally {
+            ConnectionFactory.closeConnection(conn, statement);
+        }
     }
 
-    public void update(Task task){
+    public void update(Project project){
         //Método para atualizar o banco de dados (UPDATE)
 
     }
 
-    public void removeById(int taskId) {
+    public void removeById(int projectId) {
         //Método para deletar no banco de dados(DELETE)
 
     }
 
-    public List<Task> getAll(int idProject) {
+    public List<Project> getAll(int idProject) {
         //Método para retornar todas as tarefas do banco de dados (SELECT)
         return null;
     }
