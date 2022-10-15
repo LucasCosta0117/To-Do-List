c8a3df11a28a4b0a534d51602af23ce86349b728
Lucas Costa <lucas.costa0117@gmail.com>
2022-10-15 00:49:52 -0300

    Add a new method getAll on class ProjectController

--- src/main/java/controller/ProjectController.java
+++ src/main/java/controller/ProjectController.java
@@ -4,6 +4,7 @@ import model.Project;
 import util.ConnectionFactory;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class ProjectController {
@@ -63,12 +64,54 @@ public class ProjectController {
 
     public void removeById(int projectId) {
         //Método para deletar no banco de dados(DELETE)
+        String sql = "DELETE FROM project WHERE id = ?;";
+        Connection conn = null;
+        PreparedStatement statement = null;
 
+        try {
+            conn = ConnectionFactory.getConnection();
+            statement = conn.prepareStatement(sql);
+            statement.setInt(1, projectId);
+            statement.execute();
+        } catch (Exception exception) {
+            throw new RuntimeException ("Erro ao deletar um Projeto", exception);
+        } finally {
+            ConnectionFactory.closeConnection(conn, statement);
+        }
     }
 
-    public List<Project> getAll(int idProject) {
+    public List<Project> getAll() {
         //Método para retornar todas as tarefas do banco de dados (SELECT)
-        return null;
+        String sql = "SELECT * FROM project;";
+        Connection conn = null;
+        Statement statement = null;
+        ResultSet resultSet = null;
+        List<Project> projects = new ArrayList<>();
+
+        try {
+            conn = ConnectionFactory.getConnection();
+            statement = conn.prepareStatement(sql);
+            resultSet = statement.executeQuery(sql);
+
+            while (resultSet.next()) {
+                Project project = new Project();
+
+                project.setId(resultSet.getInt("id"));
+                project.setName(resultSet.getString("nome"));
+                project.setDescription(resultSet.getString("description"));
+                project.setCreatedAt(resultSet.getDate("createdAt"));
+                project.setUpdatedAt(resultSet.getDate("updatedAt"));
+
+                projects.add(project);
+            }
+
+        } catch(Exception exception){
+            throw new RuntimeException("Erro ao exibir todos os projetos.", exception);
+        }finally {
+            ConnectionFactory.closeConnection(conn, statement, resultSet);
+        }
+
+        return projects;
     }
 
 }
