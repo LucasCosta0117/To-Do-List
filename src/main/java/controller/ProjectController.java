affb0aa367cff1e396edac76d8c100e684c5ed6e
Lucas Costa <lucas.costa0117@gmail.com>
2022-10-15 00:12:33 -0300

    Add new method update on ProjetoController

--- src/main/java/controller/ProjectController.java
+++ src/main/java/controller/ProjectController.java
@@ -36,7 +36,29 @@ public class ProjectController {
 
     public void update(Project project){
         //Método para atualizar o banco de dados (UPDATE)
+        String sql = "UPDATE project SET " +
+                "name = ?, " +
+                "description = ?, " +
+                "createdAt = ?, " +
+                "updatedAt = ? " +
+                "WHERE id = ?;";
+        Connection conn = null;
+        PreparedStatement statement = null;
 
+        try {
+            conn = ConnectionFactory.getConnection();
+            statement = conn.prepareStatement(sql);
+            statement.setString(1, project.getName());
+            statement.setString(2, project.getDescription());
+            statement.setDate(3, new Date(project.getCreatedAt().getTime()));
+            statement.setDate(4, new Date(project.getUpdatedAt().getTime()));
+            statement.setInt(5, project.getId());
+            statement.execute();
+        } catch (Exception exception) {
+            throw new RuntimeException("Erro ao atualizar os dados do projeto", exception);
+        } finally {
+            ConnectionFactory.closeConnection(conn, statement);
+        }
     }
 
     public void removeById(int projectId) {
